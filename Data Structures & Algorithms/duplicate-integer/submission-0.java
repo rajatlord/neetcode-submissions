9fcb31ebd8e994ab94857a231cfbc7f8189943c0
Rajat Mathur <143505886+rajatlord@users.noreply.github.com>
2026-07-20 08:13:38 +0530

    Bulk sync: 11 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,14 @@
+// import java.util.*;
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+
+        for(int num: nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
